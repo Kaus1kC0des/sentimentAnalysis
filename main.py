c3e8a804a883e914221e89dfde0e544f78fafd19
Kausik D <89984370+Kaus1kC0des@users.noreply.github.com>
2023-09-19 13:55:52 +0530

    Final Changes to be submitted

---
+++ main.py
@@ -0,0 +1,9 @@
+import transformers
+import torch
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification,pipeline
+
+model_path = "cardiffnlp/twitter-roberta-base-sentiment"
+sentiment = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
+
+print(sentiment("I love you"))
